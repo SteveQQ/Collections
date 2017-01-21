c45340f66fb57a1b4f680c01ae26cccc31c4a982
SteveQ <sawicki.adam172@gmail.com>
2017-01-21 18:08:01 +0100

    Inserting to structure and printing content

---
+++ src/com/steveq/BST.java
@@ -0,0 +1,202 @@
+package com.steveq;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Created by SteveQ on 2017-01-21.
+ */
+public class BST<E extends Comparable<E>> implements Collection<E>{
+
+    private int numElements;
+    private Node absRoot;
+    private Node curRoot;
+
+    public class Node implements Iterable{
+        private E data;
+        private Node leftJoin;
+        private Node rightJoin;
+        private int subtreeSize;
+
+        public Node(E data) {
+            this.data = data;
+            this.leftJoin = null;
+            this.rightJoin = null;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public Node getLeftJoin() {
+            return leftJoin;
+        }
+
+        public void setLeftJoin(Node leftJoin) {
+            this.leftJoin = leftJoin;
+        }
+
+        public Node getRightJoin() {
+            return rightJoin;
+        }
+
+        public void setRightJoin(Node rightJoin) {
+            this.rightJoin = rightJoin;
+        }
+
+        public int getSubtreeSize() {
+            return subtreeSize;
+        }
+
+        public void setSubtreeSize(int subtreeSize) {
+            this.subtreeSize = subtreeSize;
+        }
+
+        @Override
+        public Iterator iterator() {
+            return null;
+        }
+
+        @Override
+        public String toString() {
+            return data.toString();
+        }
+    }
+
+    private class BSTIterator implements Iterator{
+
+        private int cursor;
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            return null;
+        }
+    }
+
+    @Override
+    public int size() {
+        return numElements;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return numElements==0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(E e) {
+        if(absRoot == null){
+            absRoot = new Node(e);
+            return true;
+        } else {
+            curRoot = absRoot;
+            while(true){           //until reach leaf
+                if (e.compareTo(curRoot.getData()) <= 0) {
+                    if(curRoot.getLeftJoin() == null){          //empty LEFT slot found
+                        curRoot.setLeftJoin(new Node(e));
+                        return true;
+                    } else {
+                        curRoot = curRoot.getLeftJoin();
+                    }
+                } else {
+                    if(curRoot.getRightJoin() == null){         //empty RIGHT slot found
+                        curRoot.setRightJoin(new Node(e));
+                        return true;
+                    } else {
+                        curRoot = curRoot.getRightJoin();
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return traverseInOrder(absRoot);
+    }
+
+    private Object[] traverseInOrder(Node root) {
+        ArrayList<Object> result = new ArrayList<>();
+        if(root.getLeftJoin() != null){
+            result.addAll(new ArrayList<Object>(Arrays.asList(traverseInOrder(root.getLeftJoin()))));
+        }
+        result.add(root);
+        if(root.getRightJoin() != null){
+            result.addAll(new ArrayList<Object>(Arrays.asList(traverseInOrder(root.getRightJoin()))));
+        }
+        return result.toArray();
+    }
+
+    @Override
+    public String toString() {
+
+        Object[] collection = traverseInOrder(absRoot);
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+
+        for(int i=0; i < collection.length; i++){
+            builder.append(((Node)collection[i]).toString());
+            if(i < collection.length-1) {
+                builder.append(", ");
+            }
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+}
---
+++ src/com/steveq/BSTTester.java
@@ -0,0 +1,19 @@
+package com.steveq;
+
+/**
+ * Created by SteveQ on 2017-01-21.
+ */
+public class BSTTester {
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+
+        System.out.println(bst.add(10));
+        System.out.println(bst.add(8));
+        System.out.println(bst.add(12));
+        System.out.println(bst.add(24));
+        System.out.println(bst.add(18));
+        System.out.println(bst.add(27));
+        System.out.println(bst.add(5));
+        System.out.println(bst);
+    }
+}
